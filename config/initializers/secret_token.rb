a74bc432de202e57c98f1de4446f0b71d42b7bf3
Gabriel Garrido <kainlite@gmail.com>
2013-02-06 11:43:22 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Activos::Application.config.secret_token = '4bf9d5e6f77f7ae6fdf69fb57fba7aaf9eb1542a88c4002858e27a20cfa1ae48bab2233a8b9ae71e098917f36e3b795cb9295afc5dad4869f5fe604b0de7e534'
